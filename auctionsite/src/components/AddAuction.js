d32db4847f2b20087dbfb9f665e5814fe929a2bc
Freddie <freddie.runnding@yh.nackademin.se>
2019-03-29 09:26:57 +0100

    Test commit

--- auctionsite/src/components/AddAuction.js
+++ auctionsite/src/components/AddAuction.js
@@ -4,7 +4,7 @@ export default class AddAuction extends Component {
   render() {
     return (
       <div>
-        
+      
       </div>
     )
   }
--- auctionsite/src/components/Main.js
+++ auctionsite/src/components/Main.js
@@ -8,7 +8,6 @@ export default class Main extends Component {
       fontSize: "4em"
     };
     let headerStyle = {
-      width: "100%",
       display: "flex",
       justifyContent: "center"
     }
