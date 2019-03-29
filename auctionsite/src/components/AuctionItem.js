fe65fcab187514e305ef23d4c510095a30c9a26a
Sebastian Sagrelius <sagrelius@live.se>
2019-03-29 12:29:20 +0100

    added filter for loading auctions

--- auctionsite/src/components/AuctionItem.js
+++ auctionsite/src/components/AuctionItem.js
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 
 export default class AuctionItem extends Component {
   render() {
-    let allAuctions = this.props.Auctions.map(item => <div key={item.AuktionID}>{item.Titel}</div>);
-    console.log(this.props.Auctions);
-    return <React.Fragment>{allAuctions}</React.Fragment>;
+    
+    return <div><h1>{this.props.auctionItem.Titel}</h1></div>;
   }
 }
--- auctionsite/src/components/AuctionList.js
+++ auctionsite/src/components/AuctionList.js
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
 import AuctionItem from "./AuctionItem";
+import moment from "moment";
+import "moment-timezone";
+import "moment/locale/sv";
 
 export default class AuctionList extends Component {
+  
   render() {
+    let auctionListStyle = 
+    {
+      height: "90%",
+      maxHeight: "80%",
+      width: "40%",
+      background: "red"
+    }
+    let activeAuctions = this.props.Auctions.filter((item) => {
+      console.log(item.SlutDatum)
+      console.log(new Date())
+      console.log(moment(item.SlutDatum).toDate() > moment())
+      if(moment(item.SlutDatum).toDate() > moment())
+      {
+        return (item);
+      }
+      return null;
+    } );
+    let allAuctions = activeAuctions.map(item => <div key={item.AuktionID}><AuctionItem auctionItem = {item}/></div>);
+
     return (
-      <div>
-        <AuctionItem Auctions={this.props.Auctions} />
+      <div style={auctionListStyle}>
+        {allAuctions}
       </div>
     );
   }
