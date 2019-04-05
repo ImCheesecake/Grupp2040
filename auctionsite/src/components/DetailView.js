9bb2372db86af3f1d59144b152382d91243f75e5
dashbored <richard_davy@hotmail.com>
2019-04-05 13:39:00 +0200

    small change

--- auctionsite/src/components/DetailView.js
+++ auctionsite/src/components/DetailView.js
@@ -7,24 +7,26 @@ import PopupDelete from "./PopupDelete";
 import BidAuction from "./BidAuction";
 
 export default class DetailView extends Component {
-
   state = {
     bidHistory: [],
     auctionDetails: {}
-  }
+  };
 
   closeDetailView = () => {
     this.props.setDetailView(false);
-
   };
 
   updateDetailView = () => {
     this.componentDidMount();
-  }
+  };
 
   async componentDidMount() {
-    var budUrl = "https://nackowskis.azurewebsites.net/api/bud/2040/" + this.props.auctionId;    
-    var auctionUrl = "https://nackowskis.azurewebsites.net/api/Auktion/2040/" + this.props.auctionId; 
+    var budUrl =
+      "https://nackowskis.azurewebsites.net/api/bud/2040/" +
+      this.props.auctionId;
+    var auctionUrl =
+      "https://nackowskis.azurewebsites.net/api/Auktion/2040/" +
+      this.props.auctionId;
 
     var bidResp = await fetch(budUrl);
     var bidHistory = await bidResp.json();
@@ -35,46 +37,50 @@ export default class DetailView extends Component {
     await this.setState({
       bidHistory,
       auctionDetails
-    }) 
-    
+    });
   }
 
   render() {
     const styles = {
-      detailView: { 
+      detailView: {
         position: "fixed",
         top: "150px",
         left: "50%",
         width: "40%",
-        height: "400px",
+        height: "400px"
       }
     };
 
-    
     return (
-      <div style={styles.detailView} className="detailView animated fadeIn slow divCard">
+      <div
+        style={styles.detailView}
+        className="detailView animated fadeIn slow divCard"
+      >
         <i className="close" onClick={this.closeDetailView}>
           &times;
         </i>
-        <div>          
-          <ItemInfo auctionDetails={this.state.auctionDetails}/>
-          <PopupHistory bidHistory={this.state.bidHistory}/>
-          <PopupUpdate 
-          bidHistory={this.state.bidHistory} 
-          auctionDetails={this.state.auctionDetails} 
-          updateArrays={this.props.updateArrays} 
-          updateDetailView= {this.updateDetailView}/>
-          <PopupDelete 
-          bidHistory={this.state.bidHistory} 
-          auctionDetails={this.state.auctionDetails}
-          updateArrays={this.props.updateArrays}
-          hideDetailView={this.props.hideDetailView}/>
+        <div>
+          <ItemInfo auctionDetails={this.state.auctionDetails} />
+          <PopupHistory bidHistory={this.state.bidHistory} />
+          <PopupUpdate
+            bidHistory={this.state.bidHistory}
+            auctionDetails={this.state.auctionDetails}
+            updateArrays={this.props.updateArrays}
+            updateDetailView={this.updateDetailView}
+          />
+          <PopupDelete
+            bidHistory={this.state.bidHistory}
+            auctionDetails={this.state.auctionDetails}
+            updateArrays={this.props.updateArrays}
+            hideDetailView={this.props.hideDetailView}
+          />
         </div>
         <div>
-          <BidAuction 
-          auctionDetails={this.state.auctionDetails}
-          updateDetailView= {this.updateDetailView}
-          updateArrays={this.props.updateArrays} />
+          <BidAuction
+            auctionDetails={this.state.auctionDetails}
+            updateDetailView={this.updateDetailView}
+            updateArrays={this.props.updateArrays}
+          />
         </div>
       </div>
     );
