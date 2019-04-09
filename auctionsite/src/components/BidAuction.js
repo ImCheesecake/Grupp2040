4bc4055e63b169305b64b11d6a93c93409dd39a4
Sebastian Sagrelius <sagrelius@live.se>
2019-04-09 10:45:00 +0200

    add validation bid

--- auctionsite/src/components/BidAuction.js
+++ auctionsite/src/components/BidAuction.js
@@ -18,12 +18,14 @@ export default class BidAuction extends Component {
 
   handleSubmit = async e => {
     e.preventDefault();
-    if (this.state.Bud > this.state.Utropspris) {
-      await this.updateAuctionPrice();
-      this.props.updateDetailView();
-    } else {
-      alert("Idiot du kan ju inte buda lägre än maxbudet fattar du väl");
-    }
+    await this.updateAuctionPrice();
+    this.props.updateDetailView();
+    // if (this.state.Bud > this.state.Utropspris) {
+    //   await this.updateAuctionPrice();
+    //   this.props.updateDetailView();
+    // } else {
+    //   alert("Idiot du kan ju inte buda lägre än maxbudet fattar du väl");
+    // }
   };
 
   handleChange = e => {
@@ -103,6 +105,7 @@ export default class BidAuction extends Component {
                 type="number"
                 value={this.state.Bud}
                 onChange={this.handleChange}
+                min={this.state.Utropspris +1}
               />
             </div>
             <div
