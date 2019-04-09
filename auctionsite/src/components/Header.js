7586a821d62fb2dbaad25e96d12fc98404bb6fdf
Sebastian Sagrelius <sagrelius@live.se>
2019-04-09 12:30:27 +0200

    merge igen

--- auctionsite/src/components/Header.js
+++ auctionsite/src/components/Header.js
@@ -21,28 +21,14 @@ export default class Header extends Component {
         width: "75%",
         display: "flex",
         justifyContent: "space-evenly",
-<<<<<<< HEAD
         padding: ".7em 0",
         marginLeft: "35em"
-=======
-        padding: ".7em 0"
->>>>>>> sebastian
       }
     };
 
     return (
       <div style={styles.divStyle}>
-<<<<<<< HEAD
         <h1 style={styles.testStyle} className="titleText" onClick={this.handleClick}>Auction Site</h1>
-=======
-        <h1
-          style={styles.testStyle}
-          className="animated rubberBand delay-5s"
-          onClick={this.handleClick}
-        >
-          Auction Site
-        </h1>
->>>>>>> sebastian
         <Search updateAuctions={this.props.updateAuctions} />
         <Popup updateArrays={this.props.updateArrays} />
       </div>
--- auctionsite/src/components/ItemInfo.js
+++ auctionsite/src/components/ItemInfo.js
@@ -5,19 +5,12 @@ import "moment-timezone";
 export default class ItemInfo extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <div>   
       <h1>{this.props.auctionDetails.Titel}</h1>
       <h3>Description: {this.props.auctionDetails.Beskrivning}</h3>
       <h3>End date: {moment(this.props.auctionDetails.SlutDatum).format('ddd, MMM DD HH:mm')}</h3>
       
 
-=======
-      <div>
-        <h1>{this.props.auctionDetails.Titel}</h1>
-        <h3>Description: {this.props.auctionDetails.Beskrivning}</h3>
-        <h3>End date: {this.props.auctionDetails.SlutDatum}</h3>
->>>>>>> sebastian
       </div>
     );
   }
