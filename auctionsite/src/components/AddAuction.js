93e2b22903e0699a203ba1745c604cbd9e01f7db
dashbored <richard_davy@hotmail.com>
2019-04-09 12:10:46 +0200

    Updates layout for adding auctions

--- auctionsite/src/components/AddAuction.js
+++ auctionsite/src/components/AddAuction.js
@@ -72,68 +72,97 @@ export default class AddAuction extends Component {
       headerStyling: {
         marginTop: "1em",
         display: "flex",
-        justifyContent: "center"
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center"
       },
-
-      formStyling: {
-        marginTop: "7em"
+      inputStyling: {
+        margin: "10px 0 0 0",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center"
+      },
+      buttonStyling: {
+        margin: "20px 0",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
       }
     };
 
     return (
       <div style={styles.headerStyling}>
-        <h3>Add auction</h3>
-        <form onSubmit={this.handleSubmit} style={styles.formStyling}>
-          <input
-            maxLength="50"
-            value={this.state.Titel}
-            type="text"
-            name="Titel"
-            placeholder="Titel"
-            onChange={this.handleChange}
-            required
-          />
-          <input
-            maxLength="250"
-            value={this.state.Beskrivning}
-            type="text"
-            name="Beskrivning"
-            placeholder="Beskrivning"
-            onChange={this.handleChange}
-            required
-          />
-          <input
-            value={this.state.Utropspris}
-            type="number"
-            name="Utropspris"
-            placeholder="Utropspris"
-            onChange={this.handleChange}
-            min="0"
-          />
-          <input
-            maxLength="50"
-            value={this.state.SkapadAv}
-            type="text"
-            name="SkapadAv"
-            placeholder="Skapad av"
-            onChange={this.handleChange}
-            required
-          />
-          <DatePicker
-            selected={this.state.SlutDatum}
-            onChange={this.handleDateChange}
-            minDate={this.state.StartDatum}
-            maxDate={moment()
-              .add(30, "days")
-              .toDate()}
-            showTimeSelect
-            timeFormat="HH:mm"
-            timeIntervals={15}
-            dateFormat="yyyy-MM-dd HH:mm"
-            timeCaption="time"
-          />
-          <button type="submit">Skapa ny auktion</button>
-        </form>
+        <h2>Add auction</h2>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <div style={styles.inputStyling}>
+              <label htmlFor="Titel">Titel: </label>
+              <input
+                maxLength="50"
+                value={this.state.Titel}
+                type="text"
+                name="Titel"
+                placeholder="Titel"
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div style={styles.inputStyling}>
+              <label htmlFor="Beskrivning">Beskrivning: </label>
+              <textarea
+                maxLength="250"
+                value={this.state.Beskrivning}
+                type="text"
+                name="Beskrivning"
+                placeholder="Beskrivning"
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div style={styles.inputStyling}>
+              <label htmlFor="Utropspris">Utropspris: </label>
+              <input
+                value={this.state.Utropspris}
+                type="number"
+                name="Utropspris"
+                placeholder="Utropspris"
+                onChange={this.handleChange}
+                min="0"
+              />
+            </div>
+            <div style={styles.inputStyling}>
+              <label htmlFor="SkapadAv">Skapad av: </label>
+              <input
+                maxLength="50"
+                value={this.state.SkapadAv}
+                type="text"
+                name="SkapadAv"
+                placeholder="Skapad av"
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div style={styles.inputStyling}>
+              <label>Slutdatum: </label>
+              <DatePicker
+                selected={this.state.SlutDatum}
+                onChange={this.handleDateChange}
+                minDate={this.state.StartDatum}
+                maxDate={moment()
+                  .add(30, "days")
+                  .toDate()}
+                showTimeSelect
+                timeFormat="HH:mm"
+                timeIntervals={15}
+                dateFormat="yyyy-MM-dd HH:mm"
+                timeCaption="time"
+              />
+            </div>
+            <div style={styles.buttonStyling}>
+              <button type="submit">Skapa ny auktion</button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
