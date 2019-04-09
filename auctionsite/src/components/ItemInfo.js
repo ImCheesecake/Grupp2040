97d569b1fa07c77fabf808773c2d86810b5ceb77
Freddie <freddie.runnding@yh.nackademin.se>
2019-04-09 12:24:54 +0200

    css changes

--- auctionsite/src/components/ItemInfo.js
+++ auctionsite/src/components/ItemInfo.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import moment from 'moment'
+import "moment-timezone";
 
 export default class ItemInfo extends Component {
   render() {
@@ -6,7 +8,7 @@ export default class ItemInfo extends Component {
       <div>   
       <h1>{this.props.auctionDetails.Titel}</h1>
       <h3>Description: {this.props.auctionDetails.Beskrivning}</h3>
-      <h3>End date: {this.props.auctionDetails.SlutDatum}</h3>
+      <h3>End date: {moment(this.props.auctionDetails.SlutDatum).format('ddd, MMM DD HH:mm')}</h3>
       
 
       </div>
